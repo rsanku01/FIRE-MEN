219d311775468b57aaf97dac9bffd02b36c09dfd
NN <100281638+Theindianstyles@users.noreply.github.com>
2022-06-24 10:46:52 +0200

    Update 1716.ino

--- NAT/1716.ino
+++ NAT/1716.ino
@@ -5,6 +5,7 @@
 #include <PubSubClient.h>
 #include <stdio.h>
 
+
 char data;
 char OutputPump;
 float DerivedValue, IntegralValue;
@@ -14,7 +15,6 @@ char GetPatameters[50];
 int n = 0;
 float pv=20;
 float cp=20;
-float Kp=20;
 int on_off = 1;
 char UpdateParameters[50];
 float GetPar = 0;
@@ -26,8 +26,8 @@ const char* ssid = "udcdocencia";
 const char* password = "Universidade.2022";
 WiFiClient espClient;
 PubSubClient client(espClient);
-const char* mqtt_server = "10.20.30.244";                                //get the ifconfig and change it
-const char* plant = "plant3";                                           //check the plant id and change for every plant
+const char* mqtt_server = "10.20.29.212";                                   //get the ifconfig and change it
+const char* plant = "plant3";                                              //check the plant id and change for every plant
 const char* topic_update_parameters = "plant3/update_parameters";
 const char* topic_get_parameters = "plant3/get_parameters";
 const char* topic_on_off = "plant3/on_off";
@@ -35,10 +35,9 @@ const char* topic_data = "plant3/data";
 const char* topic_parameters = "plant3/parameters";
 
 
-
 void setup_wifi() {
   delay(10);
-  WiFi.begin ("udcdocencia", "Universidade.2022");            //check if quotes are required for connection
+  WiFi.begin ("udcdocencia", "Universidade.2022");                        //connect the board with internet
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -47,11 +46,7 @@ void setup_wifi() {
 }
 
 
-
-
-
-
-float error;
+float error;                                                             // PID
 float v;
 float x;
 
@@ -77,6 +72,7 @@ class PID
       update_constants(kp, ti, td);
     }
 };
+
 
 float PID::get_control_action(float error_k) {
   integral = (c1 * (error_k + error)) + integral;
@@ -109,9 +105,11 @@ void PID::update_constants (float k_, float ti_, float td_)
 
 PID nat(10, 20, 5.50, 1);
 
-void callback (char* topic, byte* payload, unsigned int length) {
 
-  if (String(topic) == topic_update_parameters) {
+void callback (char* topic, byte* payload, unsigned int length) {                    //loop of the callbacks
+
+  
+  if (String(topic) == topic_update_parameters) {                                    //if command to update parameters
     char inmsg[length + 1];
     for (int i = 0; i < length; i++) {
       inmsg[i] = (char)payload[i];
@@ -123,7 +121,7 @@ void callback (char* topic, byte* payload, unsigned int length) {
     client.subscribe(topic_update_parameters);
   }
 
-  if (String(topic) == topic_get_parameters) {
+  if (String(topic) == topic_get_parameters) {                                       //if command to get the parameters
     char inmsg2[length + 1];
     for (int i = 0; i < length; i++) {
       inmsg2[i] = (char)payload[i];
@@ -136,11 +134,8 @@ void callback (char* topic, byte* payload, unsigned int length) {
     client.publish(topic_parameters, Send_Parameters, topic_data);
   }
 
-  if (String(topic) == topic_on_off) {
+  if (String(topic) == topic_on_off) {                                              //if command to publish on and off 
     char inmsg3[length + 1];
-
-
-
     for (int i = 0; i < length; i++) {
       inmsg3[i] = (char)payload[i];              //check if it works with1 since its either 1 or 0
     }
@@ -150,50 +145,44 @@ void callback (char* topic, byte* payload, unsigned int length) {
     client.publish(topic_parameters, Send_Parameters, topic_data);
     client.subscribe(topic_on_off);
   }
-  //  inmsg3[length] = '\0';
-  //  on_off = inmsg3[0];
-  //  mqtt_in_onoff = String(inmsg3) == "1";
-  //  }
+  
 
-  if (String(topic) == topic_data) {
+  if (String(topic) == topic_data) {                                               //if command to subscribe to the data
     char inmsg4[length + 1];
     for (int i = 0; i < length; i++) {
       inmsg4[i] = (char)payload[i];
     }
-    inmsg4[length] = '\0';
-
-
-    snprintf(inmsg4, "%f;%f;%f;%f", SP, pv, cp, Time);
-    client.publish(topic_data);
+    char Send_Parameters[50];
+    snprintf(Send_Parameters, 50, "%f;%f;%f;%f;%i", SP, nat.kp, nat.ti, nat.td, on_off);
     client.subscribe(topic_data);
 
   }
 
 
-  if (String(topic) == topic_parameters) {
+  if (String(topic) == topic_parameters) {                                       //if command to subscribe to the parameters
     char inmsg5[length + 1];
     for (int i = 0; i < length; i++) {
-      inmsg[i] = (char)payload[i];
+      inmsg5[i] = (char)payload[i];
     }
-    inmsg5[length] = '\0';
-    float Kp, Ti, Td;
-    snprintf(inmsg, "%f;%f;%f;%f;%i", &SP, &Kp, &Ti, &Td, &on_off);
-   // client.publish(topic_parameters);
+    float kp, ti, td;
+    char yyyy[50];
+    snprintf(yyyy,50, "%f;%f;%f;%f;%i", SP, nat.kp, nat.ti, nat.td, on_off);
     client.subscribe(topic_parameters);
   }
 
 }
 
+
 void setup() {
   pinMode(D3, OUTPUT);
   Serial.begin(115200);
   setup_wifi();
-  client.setServer("10.20.30.244", 1883);                     //change the mqtt server as required by checking ifconfig
+  client.setServer("10.20.29.212", 1883);                                          //change the mqtt server as required by checking ifconfig
   client.connect(plant);
   client.setCallback(callback);
-  //  client.subscribe(data);
   client.loop();
 }
+
 
 void loop() {
   analogRead(A0);
